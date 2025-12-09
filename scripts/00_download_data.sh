7e052aee1ff99c8bc164754829d2b32d27f39156
Sergey Dadoyan <145821999+sda98@users.noreply.github.com>
2025-12-08 18:05:54 -0700

    Update 00_download_data.sh

--- scripts/00_download_data.sh
+++ scripts/00_download_data.sh
@@ -12,46 +12,6 @@ mkdir -p "$RAW_DIR"
 echo "Downloading RNA-seq data into: ${RAW_DIR}"
 echo
 
-###############################################################################
-# OPTION 1: Download FASTQ files directly with wget/curl (e.g., from ENA/GEO)
-###############################################################################
-# Uncomment and edit this section if you have direct URLs to FASTQ files.
-#
-# Example:
-#   SAMPLE_IDS=("sample1" "sample2" "sample3" "sample4")
-#   FASTQ_URLS_R1=("URL_for_sample1_R1" "URL_for_sample2_R1" ...)
-#   FASTQ_URLS_R2=("URL_for_sample1_R2" "URL_for_sample2_R2" ...)
-#
-# SAMPLE_IDS=("sample1" "sample2")
-# FASTQ_URLS_R1=(
-#   "https://example.org/path/to/sample1_R1.fastq.gz"
-#   "https://example.org/path/to/sample2_R1.fastq.gz"
-# )
-# FASTQ_URLS_R2=(
-#   "https://example.org/path/to/sample1_R2.fastq.gz"
-#   "https://example.org/path/to/sample2_R2.fastq.gz"
-# )
-#
-# for i in "${!SAMPLE_IDS[@]}"; do
-#   SAMPLE="${SAMPLE_IDS[$i]}"
-#   URL_R1="${FASTQ_URLS_R1[$i]}"
-#   URL_R2="${FASTQ_URLS_R2[$i]}"
-#
-#   echo "Downloading ${SAMPLE}..."
-#   wget -O "${RAW_DIR}/${SAMPLE}_R1.fastq.gz" "${URL_R1}"
-#   wget -O "${RAW_DIR}/${SAMPLE}_R2.fastq.gz" "${URL_R2}"
-# done
-#
-# echo "Download finished (Option 1)."
-# exit 0
-
-###############################################################################
-# OPTION 2: Download from SRA using the SRA Toolkit (prefetch + fasterq-dump)
-###############################################################################
-# Uncomment and edit this section if you want to pull data by SRA accessions.
-# Requires: prefetch, fasterq-dump (from SRA Toolkit) in your PATH.
-###############################################################################
-
 # List of SRA run accessions (replace with real ones)
 SRA_IDS=(
   "SRRXXXXXXX"  # replace with real SRA ID
